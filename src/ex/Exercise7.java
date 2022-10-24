ef1ad92a025a73d7331d68f6f0e779fdb1c7e397
Christopher Wu <christopher.wu@aa.com>
2022-10-23 20:20:55 -0500

    exercise 7

---
+++ src/ex/Exercise7.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+	
+		for(int a = 1; a < 11; a++) {
+			int result = 8 * a;
+			System.out.println("8 * " + a + " = " + result);
+	
+		}
+	
+	}
+}
